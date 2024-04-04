3da5482a3d340b82da55893363e946a2fe4f6e41
Nick Wagner <nwagner@getnerdio.com>
2024-04-04 12:32:34 -0500

    updated to support instance variable

--- scripted-actions/windows-scripts/Install Threatlocker agent.ps1
+++ scripted-actions/windows-scripts/Install Threatlocker agent.ps1
@@ -4,26 +4,40 @@
 
 <#
 Notes:
-The installation script requires an organization name and ThreatLocker Unique Identifier.
+The installation script requires an organization name, ThreatLocker Unique Identifier,
+and a ThreatLocker Instance.
 
 Unique Identifier can be found in Deployment Center under the RMM and Script Deployment section. 
 https://threatlocker.kb.help/getting-your-unique-identifier-from-threatlocker/
+
+The ThreatLocker instance can be found in the ThreatLocker portal URL, under Help. Find
+"ThreatLocker Access" will be followed by a letter. This letter is the instance. For example,
+
+ThreatLocker Access (G) - The instance is "g". G is the default instance; if not specified, 
+the script will use "g".
 
 You must provide secure variables to this script as seen in the Required Variables section. 
 Set these up in Nerdio Manager under Settings->Portal. The variables to create are:
     ThreatlockerOrgName
     ThreatlockerUniqueId
+    ThreatlockerInstance
 #>
 
 ##### Required Variables #####
 
 $ThreatlockerOrgName = $SecureVars.ThreatlockerOrgName;
 $ThreatlockerIdentifier = $SecureVars.ThreatlockerUniqueId;
+$ThreatlockerInstance = $SecureVars.ThreatlockerInstance;
 
 ##### Variables #####
 
 #Set Group Name 
 $ThreatlockerGroupName = "Workstations";
+
+#Set Threatlocker Instance
+if ([string]::IsNullOrEmpty($ThreatlockerInstance)) {
+    $ThreatlockerInstance = "g";
+}
 
 ##### Script Logic #####
 
@@ -32,7 +46,7 @@ if(($ThreatlockerOrgName -eq $null) -and  ($ThreatlockerIdentifier -eq $null))  
 }
 
 try {
-    $Url = 'https://api.threatlocker.com/getgroupkey.ashx'; 
+    $Url = "https://api.$ThreatlockerInstance`.threatlocker.com/getgroupkey.ashx"; 
     $Headers = @{'Authorization'=$ThreatlockerIdentifier;'OrganizationName'=$ThreatlockerOrgName;'GroupName'=$ThreatlockerGroupName}; 
     
     [Net.ServicePointManager]::SecurityProtocol = [Enum]::ToObject([Net.SecurityProtocolType], 3072)
@@ -40,7 +54,8 @@ try {
     $GroupId = $response.split([Environment]::NewLine)[0].split(':')[1].trim();
 }
 catch {
-    Write-Output "Failed to get GroupId";
+    Write-Output "Failed to get GroupId. Check the Notes section for information about setting the ThreatLocker instance.";
+    throw $_;
     Exit 1;
 }
 
