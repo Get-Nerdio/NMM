97f9092a46259f90ffcad5d68b71fa210352297b
Losev Vyatcheslav <slosev@getnerdio.com>
2024-07-08 13:29:46 +0300

    [#25671] used Write-Log to save logs to file

--- scripted-actions/windows-scripts/Install Microsoft Teams.ps1
+++ scripted-actions/windows-scripts/Install Microsoft Teams.ps1
@@ -12,17 +12,41 @@ This script performs the following:
 #>
 
 # Start powershell logging
+function Write-Log {
+    param
+    (
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [string]$Message
+    )
+    try {
+        Write-Host $Message
+        $tempFolder = [environment]::GetEnvironmentVariable('TEMP', 'Machine')
+        $logsFolderName = "NerdioManagerLogs\ScriptedActions\msteams"
+        $logsPath = "$tempFolder\$logsFolderName"
+  
+        if (-not (Test-Path -Path $logsPath)) {
+            New-Item -Path $tempFolder -Name $logsFolderName -ItemType Directory -Force | Out-Null
+        }
+  
+        $DateTime = Get-Date -Format "MM-dd-yy HH:mm:ss"
+        if ($Message) {
+            Add-Content -Value "$DateTime - $Message" -Path "$logsPath\ps_log.txt"
+        }
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+}
+
 $SaveVerbosePreference = $VerbosePreference
 $VerbosePreference = 'continue'
 $VMTime = Get-Date
 $LogTime = $VMTime.ToUniversalTime()
-mkdir "C:\Windows\temp\NerdioManagerLogs\ScriptedActions\msteams" -Force
-Start-Transcript -Path "C:\Windows\temp\NerdioManagerLogs\ScriptedActions\msteams\ps_log.txt" -Append
-Write-Host "################# New Script Run #################"
-Write-host "Current time (UTC-0): $LogTime"
+Write-Log "################# New Script Run #################"
+Write-Log "Current time (UTC-0): $LogTime"
 
 # set registry values for Teams to use VDI optimization 
-Write-Host "INFO: Adjusting registry to set Teams to WVD Environment mode" -ForegroundColor Gray
+Write-Log "INFO: Adjusting registry to set Teams to WVD Environment mode"
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Teams /v "IsWVDEnvironment" /t REG_DWORD /d 1 /f
 
 # uninstall any previous versions of MS Teams or Web RTC
@@ -31,16 +55,16 @@ $TeamsPath = [System.IO.Path]::Combine($env:LOCALAPPDATA, 'Microsoft', 'Teams')
 $TeamsUpdateExePath = [System.IO.Path]::Combine($env:LOCALAPPDATA, 'Microsoft', 'Teams', 'Update.exe')
 try {
     if ([System.IO.File]::Exists($TeamsUpdateExePath)) {
-        Write-Host "INFO: Uninstalling Teams process (per-user installation)"
+        Write-Log "INFO: Uninstalling Teams process (per-user installation)"
 
         # Uninstall app
         $proc = Start-Process $TeamsUpdateExePath "-uninstall -s" -PassThru
         $proc.WaitForExit()
     }
     else {
-        write-host "INFO: No per-user teams install found."
+        Write-Log "INFO: No per-user teams install found."
     }
-    Write-Host "INFO: Deleting any possible Teams directories (per user installation)."
+    Write-Log "INFO: Deleting any possible Teams directories (per user installation)."
     Remove-Item -path $TeamsPath -recurse -ErrorAction SilentlyContinue
 }
 catch  {
@@ -51,14 +75,14 @@ catch  {
 $GetTeams = get-wmiobject Win32_Product | Where-Object IdentifyingNumber -match "{731F6BAA-A986-45A4-8936-7C3AAAAA760B}"
 if ($null -ne $GetTeams){
     Start-Process C:\Windows\System32\msiexec.exe -ArgumentList '/x "{731F6BAA-A986-45A4-8936-7C3AAAAA760B}" /qn /norestart' -Wait 2>&1
-    Write-Host "INFO: Teams per-machine Install Found, uninstalling teams"
+    Write-Log "INFO: Teams per-machine Install Found, uninstalling teams"
 }
 
 # WebRTC uninstall logic
 $GetWebRTC = get-wmiobject Win32_Product | Where-Object IdentifyingNumber -match "{FB41EDB3-4138-4240-AC09-B5A184E8F8E4}"
 if ($null -ne $GetWebRTC){
     Start-Process C:\Windows\System32\msiexec.exe -ArgumentList '/x "{FB41EDB3-4138-4240-AC09-B5A184E8F8E4}" /qn /norestart' -Wait 2>&1
-    Write-Host "INFO: WebRTC Install Found, uninstalling Current version of WebRTC"
+    Write-Log "INFO: WebRTC Install Found, uninstalling Current version of WebRTC"
 }
 
 # make directories to hold new install 
@@ -69,7 +93,7 @@ $DLink = "https://teams.microsoft.com/downloads/desktopurl?env=production&plat=w
 Invoke-WebRequest -Uri $DLink -OutFile "C:\Windows\Temp\msteams_sa\install\Teams_windows_x64.msi" -UseBasicParsing
 
 # use installer to install Machine-Wide
-Write-Host "INFO: Installing MS Teams"
+Write-Log "INFO: Installing MS Teams"
 Start-Process C:\Windows\System32\msiexec.exe `
 -ArgumentList  '/i C:\Windows\Temp\msteams_sa\install\Teams_windows_x64.msi /l*v C:\Windows\temp\NerdioManagerLogs\ScriptedActions\msteams\teams_install_log.txt ALLUSER=1 ALLUSERS=1 /qn /norestart' -Wait 2>&1
 
@@ -80,12 +104,10 @@ $dlink2 = "https://aka.ms/msrdcwebrtcsvc/msi"
 Invoke-WebRequest -Uri $DLink2 -OutFile "C:\Windows\Temp\msteams_sa\install\MsRdcWebRTCSvc_x64.msi" -UseBasicParsing
 
 # install Teams WebRTC Websocket Service
-Write-Host "INFO: Installing WebRTC component"
+Write-Log "INFO: Installing WebRTC component"
 Start-Process C:\Windows\System32\msiexec.exe `
 -ArgumentList '/i C:\Windows\Temp\msteams_sa\install\MsRdcWebRTCSvc_x64.msi /l*v C:\Windows\temp\NerdioManagerLogs\ScriptedActions\msteams\WebRTC_install_log.txt /qn /norestart' -Wait 2>&1
-Write-Host "INFO: Finished running installers. Check C:\Windows\Temp\msteams_sa for logs on the MSI installations."
-Write-Host "INFO: All Commands Executed; script is now finished. Allow 5 minutes for teams to appear" -ForegroundColor Green
+Write-Log "INFO: Finished running installers. Check C:\Windows\Temp\msteams_sa for logs on the MSI installations."
+Write-Log "INFO: All Commands Executed; script is now finished. Allow 5 minutes for teams to appear"
 
-# End Logging
-Stop-Transcript
 $VerbosePreference=$SaveVerbosePreference
--- scripted-actions/windows-scripts/Install Zoom VDI client.ps1
+++ scripted-actions/windows-scripts/Install Zoom VDI client.ps1
@@ -11,14 +11,39 @@ To install specific versions, update the URL variables below with links to the .
 $ZoomClientUrl= "https://zoom.us/download/vdi/5.17.6.24660/ZoomInstallerVDI.msi?archType=x64"
 
 # Start powershell logging
+function Write-Log {
+    param
+    (
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true)]
+        [string]$Message
+    )
+    try {
+        Write-Host $Message
+        $tempFolder = [environment]::GetEnvironmentVariable('TEMP', 'Machine')
+        $logsFolderName = "NerdioManagerLogs\ScriptedActions\zoom_sa"
+        $logsPath = "$tempFolder\$logsFolderName"
+  
+        if (-not (Test-Path -Path $logsPath)) {
+            New-Item -Path $tempFolder -Name $logsFolderName -ItemType Directory -Force | Out-Null
+        }
+  
+        $DateTime = Get-Date -Format "MM-dd-yy HH:mm:ss"
+        if ($Message) {
+            Add-Content -Value "$DateTime - $Message" -Path "$logsPath\ps_log.txt"
+        }
+    }
+    catch {
+        Write-Error $_.Exception.Message
+    }
+}
+
 $SaveVerbosePreference = $VerbosePreference
 $VerbosePreference = 'continue'
 $VMTime = Get-Date
 $LogTime = $VMTime.ToUniversalTime()
-mkdir "$env:temp\NerdioManagerLogs\ScriptedActions\zoom_sa" -Force
-Start-Transcript -Path "$env:temp\NerdioManagerLogs\ScriptedActions\zoom_sa\ps_log.txt" -Append
-Write-Host "################# New Script Run #################"
-Write-host "Current time (UTC-0): $LogTime"
+
+Write-Log "################# New Script Run #################"
+Write-Log "Current time (UTC-0): $LogTime"
 
 # Make directory to hold install files
 mkdir "$env:temp\zoom_sa\install" -Force
@@ -26,11 +51,9 @@ mkdir "$env:temp\zoom_sa\install" -Force
 Invoke-WebRequest -Uri $ZoomClientUrl -OutFile "$env:temp\zoom_sa\install\ZoomInstallerVDI.msi" -UseBasicParsing
 
 # Install Zoom. Edit the argument list as desired for customized installs: https://support.zoom.us/hc/en-us/articles/201362163
-Write-Host "INFO: Installing Zoom client. . ."
+Write-Log "INFO: Installing Zoom client. . ."
 Start-Process C:\Windows\System32\msiexec.exe `
 -ArgumentList "/i $env:temp\zoom_sa\install\ZoomInstallerVDI.msi /l*v $env:temp\NerdioManagerLogs\ScriptedActions\zoom_sa\zoom_install_log.txt /qn /norestart" -Wait
-Write-Host "INFO: Zoom client install finished."
+Write-Log "INFO: Zoom client install finished."
 
-# End Logging
-Stop-Transcript
 $VerbosePreference=$SaveVerbosePreference
